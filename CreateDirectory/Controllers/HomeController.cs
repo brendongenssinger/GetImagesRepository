d41dabd8cba2642972cf9d8b50d0abcc4a614f1d
Brendon Genssinger Almeida mascarenhas <brendon.genssinger@gmail.com>
2017-04-17 12:58:30 -0300

    Add project files.

---
+++ CreateDirectory/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Web.Mvc;
+using System.IO;
+using System;
+using CreateDirectory.Models;
+using System.Collections.Generic;
+namespace CreateDirectory.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        int id = 0;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public HomeController()
+        {
+            getImagens();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Clientes cliente)
+        {
+            try
+            {
+                foreach (string filename in Request.Files)
+                {
+                    id++;
+                    var fileName = Request.Files[filename];
+                    var path = Server.MapPath("~/App_Data/Imagens/" + id);
+                    var pathImage = Path.Combine(Server.MapPath("/App_Data/Imagens/" + id), fileName.FileName);
+
+                    if (!Directory.Exists("~/App_Data/Imagens/" + id))
+                    {
+                        Directory.CreateDirectory(path);
+                        fileName.SaveAs(pathImage);
+                    }
+                    else
+                    {
+                        fileName.SaveAs(path);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }          
+         
+
+            return View();
+
+        }
+
+        public ActionResult getImagens()
+        {
+            Clientes ci = new Clientes();
+            DirectoryInfo diretorio = new DirectoryInfo("~/App_Data/Imagens/1");            
+            FileInfo[] arquivos = diretorio.GetFiles();
+            foreach (var item in arquivos)
+            {
+                ci.NameImagens.Add(item.FullName);
+            }
+            return Index();
+        }
+    }
+}
---
+++ CreateDirectory/Models/Clientes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace CreateDirectory.Models
+{
+    public class Clientes
+    {
+        public int ID { get; set; }
+        private int quantidadeImagem { get; set; }
+        public int imagem
+        {
+            get
+            {
+                return quantidadeImagem = 1;
+            }
+            set
+            {
+                quantidadeImagem = 1;
+            }
+        }
+
+        public List<string> NameImagens { get; set; }
+    }
+}
